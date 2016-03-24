4c2c4ca9428f0f1fad4628542fe17bb1c2e38188
Alexander Pirnarev <sashopirnarev@gmail.com>
2016-03-24 17:09:09 +0200

    add update feature for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
@@ -7,11 +12,27 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = "You have signed up successfully"
-      redirect_to root_path
+      redirect_to @user
     else
       flash[:error] = "Error in fields."
       render :new
     end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:notice] = 'Profile updated!'
+      redirect_to @user
+    else
+      flash[:danger] = "Error in fields."
+      render :edit
+    end
+    debugger
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :email, presence: true
   validates :name, presence: true
-  validates :password, presence: true
+  validates :password, presence: true, allow_nil: true
 end
